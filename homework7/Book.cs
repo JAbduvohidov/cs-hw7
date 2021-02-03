f38ac86f973cfdc64a0759d061931f4723c2c2df
jabduvohidov <j.abduvohidov@team.alif.tj>
2021-02-03 10:41:23 +0500

    initial version

---
+++ homework7/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace homework7
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string About { get; set; }
+        public uint Pages { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
